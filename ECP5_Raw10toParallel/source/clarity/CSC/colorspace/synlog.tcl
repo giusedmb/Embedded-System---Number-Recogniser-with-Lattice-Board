08111fe230c394f2b6d4ea924a433ebc7a8fc880
GabrieleBusacca <gabriele.busacca@mail.polimi.it>
2026-06-10 20:35:46 +0200

    create directory fpga_hardware

--- ECP5_Raw10toParallel/source/clarity/CSC/colorspace/synlog.tcl
+++
@@ -1,1 +0,0 @@
-project -load C:/Users/crobinso/Documents/Training/FAEdec2017/LAB2/ECP5_Raw10toParallel_sensor/rtl/clarity/CSC/colorspace/syn_results/colorspace.prj
--- ECP5_Raw10toParallel/source/clarity/image_fifo/image_fifo_tmpl.v
+++
@@ -1,4 +0,0 @@
-//Verilog instantiation template
-
-image_fifo _inst (.sc_fifo_Data(), .sc_fifo_Q(), .sc_fifo_Clock(), .sc_fifo_Empty(), 
-           .sc_fifo_Full(), .sc_fifo_RdEn(), .sc_fifo_Reset(), .sc_fifo_WrEn());
